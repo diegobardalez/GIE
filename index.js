aa1e27675704f681a1041421ba588037746bf0a6
diegobardalez <2018043198@unfv.edu.pe>
2023-08-11 14:15:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,28 @@
 
 // Función para aplicar los estilos al título y fondo
 function aplicarEstilos() {
-  // Cambiar el fondo a negro
-  document.body.style.backgroundColor = 'black';
+  // Crear un elemento div para el fondo negro
+  const fondoNegro = document.createElement('div');
+  fondoNegro.style.backgroundColor = 'black';
+  fondoNegro.style.position = 'fixed';
+  fondoNegro.style.top = '0';
+  fondoNegro.style.left = '0';
+  fondoNegro.style.width = '100%';
+  fondoNegro.style.height = '100%';
+  fondoNegro.style.zIndex = '9999'; // Asegura que esté por encima de todos los elementos
+  document.body.appendChild(fondoNegro);
 
   // Crear un elemento h1 para el título
   const titulo = document.createElement('h1');
   titulo.textContent = 'GIE DDLR';
-  
-  // Aplicar estilos al título
   titulo.style.color = 'limegreen';
   titulo.style.fontSize = '36px';
   titulo.style.textAlign = 'center';
-
-  // Agregar el título al cuerpo del documento
+  titulo.style.position = 'fixed';
+  titulo.style.top = '50%';
+  titulo.style.left = '50%';
+  titulo.style.transform = 'translate(-50%, -50%)';
+  titulo.style.zIndex = '10000'; // Asegura que esté por encima de todo
   document.body.appendChild(titulo);
 }
 
